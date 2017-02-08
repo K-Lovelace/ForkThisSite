d0600db76017d61622ed85c9dbe20439a7388d37
K-Lovelace <gael.cuminal@hotmail.fr>
2017-02-08 17:49:49 +0100

    init app

---
+++ src/ForkThisSite.java
@@ -0,0 +1,63 @@
+/**
+ * Created by lovelacez on 2/8/17.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ForkThisSite extends Application {
+    private Stage primaryStage;
+    private BorderPane layout;
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+    
+        initLayout();
+    
+        showHome();
+    }
+    
+    /**
+     * Initializes the root layout.
+     */
+    private void initLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ForkThisSite.class.getResource("view/layout.fxml"));
+            layout = loader.load();
+            
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(layout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void showHome() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ForkThisSite.class.getResource("view/home.fxml"));
+            AnchorPane home = loader.load();
+            
+            // Set person overview into the center of root layout.
+            layout.setCenter(home);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
