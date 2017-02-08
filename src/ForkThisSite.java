f11196266ac57a78a7d28f9c093bbda87036b2dc
K-Lovelace <gael.cuminal@hotmail.fr>
2017-02-08 18:29:01 +0100

    layout and home working (kinda)

--- src/ForkThisSite.java
+++ src/ForkThisSite.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
